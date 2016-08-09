9a5f00d33076e416f1394b6ff3bcada63be93eb6
Thomas N Rizzo III <thomasnrizzoiii@Thomass-MacBook-Pro.local>
2016-08-09 09:26:40 -0600

    Added API, reduced level difficulty

---
+++ API.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("button").click(function() {
+        $.ajax({
+            url: 'https://randomuser.me/api/',
+            success: function(data) {
+                console.log(data.results[0].name.first);
+                $("#div1").html(data.results[0].name.first);
+            }
+        });
+    });
+});
